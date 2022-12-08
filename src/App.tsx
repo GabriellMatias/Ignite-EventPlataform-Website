46719113844d754f5f536768f00f56db5aabfcdf
GabriellMatias <gabriel.goiasat@gmail.com>
2022-12-08 15:24:52 -0300

    :triangular_flag_on_post: First commit, updating files

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,39 @@
+import {gql, useQuery} from '@apollo/client'
 
 
+const GET_LESSONS_QUERY = gql`
+  query{
+    lessons{
+      id
+      title
+      teacher{
+        name
+      }
+    }
+  }
+`
 export default function App() {
+  const { data } = useQuery(GET_LESSONS_QUERY)
+  console.log(data);
+  
+
+  /* utilizando o useQuery e a mesma coisa que utilizar o useEffect, no entanto 
+  menos codigo e mais performace*/
+
+  // useEffect((()=>{
+  //   client.query({
+  //     query:GET_LESSONS_QUERY
+  //   }).then( response => console.log(response.data)
+  //   )
+  // }), [])
 
   return (
-    <div>app</div>
+    <ul>
+
+      {data?.lessons.map(lesson =>{ 
+        return <li>{lesson.title}</li>
+      })}
+    </ul>
   )
 }
 
